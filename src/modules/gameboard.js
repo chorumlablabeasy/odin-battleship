9b88978cdd40065493880f08b89a33da8826fb36
14mudanuni14 <chorumlablabeasy@hotmail.com>
2025-12-16 02:51:41 +0300

    Gameboard'a createMap fonksiyonunu ekle

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,3 +1,23 @@
 class Gameboard {
-  
+  constructor() {
+    this.map = createMap(10)
+    this.shipObjects = []
+    this.unhittableSquares = new Set()
+  }
+}
+
+function createMap(n) {
+  const map = []
+
+  for (let i = 0; i < n; i++) {
+    const row = []
+
+    for (let m = 0; m < n; m++) {
+      row.push(null)
+    }
+
+    map.push(row)
+  }
+
+  return map
 }
